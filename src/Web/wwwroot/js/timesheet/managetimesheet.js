d9aa4146523ad9fe19ad658b577fd76f91784550
vwn王娜(Bill Wang) <wang.na@Ctrip.com>
2017-06-08 17:43:02 +0800

    use vue for manange timesheet page

--- src/Web/wwwroot/js/timesheet/managetimesheet.js
+++ src/Web/wwwroot/js/timesheet/managetimesheet.js
@@ -9,40 +9,80 @@
             }
         });
     });
-});
 
-(function () {
-    angular.module('manageTimeSheet', [])
-        .controller('manageTimeSheetCtrl', function ($scope, $http) {
-            $scope.users = [];
-            $scope.names = {};
-            $scope.currentTextIndex = {};
-            $scope.showText = {};
-
-            $scope.showTexts = ["Show All", "Show Recently"];
-
+    var users = new Vue({
+        el: '#users',
+        created: function () {
             var url = _basePath + "/api/TimeSheet/GetManageTimeSheetModel";
-            $http.post(url).then(function (response) {
-                $scope.users = response.data;
+            this.$http.post(url).then(response => {
+                this.showUsers[0] = response.data;
 
-                $.each($scope.users, function (key, value) {
-                    $scope.currentTextIndex[key] = 0;
-                    $scope.showText[key] = $scope.showTexts[0];
-                });
-            });
+                this.showUsers[1] = {};
+                var data = response.data;
+                for (var userId in data) {
+                    this.$set(this.text, userId, this.showTexts[1]);
+                    this.text[userId] = this.showTexts[1];
+                    this.showUsers[1][userId] = [];
+                    for (var rowId in data[userId]) {
+                        if (data[userId][rowId].Status !== "Done") {
+                            var item = {};
+                            $.extend(item, data[userId][rowId])
+                            this.showUsers[1][userId].push(item);
+                        }
+                    }
+                }
 
+                for (var userId in this.showUsers[1]) {
+                    this.$set(this.users, userId, this.showUsers[1][userId]);
+                }
+            }, response => { });
+
+            var names = this.names;
             url = _basePath + "/api/user/GetUserNames";
-            $http.post(url).then(function (response) {
-                $scope.names = response.data;
-            });
+            this.$http.post(url).then(response => {
+                this.names = response.data;
+            }, response => { });
+        },
+        data: {
+            showUsers: {
+                0: {},
+                1: {}
+            },
+            users: {},
+            names: {},
+            showTexts: {
+                0: "Show All",
+                1: "Show Recently"
+            },
+            text: {}
+        },
+        methods: {
+            switchShowAll: function (userId) {
+                if (this.text[userId] === this.showTexts[0]) {
+                    //this.$set(this.text, userId, this.showTexts[1]);
+                    //this.$set(this.users, userId, this.showUsers[1][userId]);
 
-            $scope.switchShowAll = function (userId) {
-                $scope.currentTextIndex[userId] = ($scope.currentTextIndex[userId] + 1) % 2;
-                $scope.showText[userId] = $scope.showTexts[$scope.currentTextIndex[userId]];
-            };
+                    this.text[userId] = this.showTexts[1];
+                    this.users[userId] = this.showUsers[1][userId];
+                }
+                else {
+                    //this.$set(this.text, userId, this.showTexts[1]);
+                    //this.$set(this.users, userId, this.showUsers[1][userId]);
 
-            $scope.getTimeSheetId = function (monday) {
+                    this.text[userId] = this.showTexts[0];
+                    this.users[userId] = this.showUsers[0][userId];
+                }
+                //this.$set(this.currentTextIndex, userId, index);
+                //this.currentTextIndex = Object.assign({}, this.currentTextIndex, { userId: index });
+                //this.$set(this.showText, userId, this.showTexts[index]);
+                //this.showText = Object.assign({}, this.showText, { userId: this.showTexts[index] });
+            },
+            getTimeSheetId: function (monday) {
                 return utility.getTimeSheetId(monday);
-            };
-        });
-}());
+            },
+            editOrRead: function (status, monday, userId) {
+                return "TimeSheet/" + status === "Done" ? "Read" : "Edit" + "TimeSheet?monday=" + this.getTimeSheetId(monday) + "&userId=" + userId;
+            }
+        }
+    })
+});
