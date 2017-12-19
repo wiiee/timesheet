a0688ce978d72256bf5c60f90ece169fd18f0f20
wiiee@163.com <wiiee@163.com>
2017-12-19 23:32:05 +0800

    update code

---
+++ src/Web/wwwroot/js/report/reportbyperformance/leader.js
@@ -0,0 +1,77 @@
+﻿$(function () {
+    $("body").on("click", "a.return", function (evt) {
+        //evt.stopPropagation();
+        evt.stopImmediatePropagation();
+        var element = $(this);
+        bootbox.confirm("Are you sure to return this timesheet?", function (result) {
+            if (result) {
+                location = _basePath + "/TimeSheet/ReturnTimeSheet?monday=" + element.attr("data-monday") + "&userId=" + element.attr("data-userId");
+            }
+        });
+    });
+
+    var users = new Vue({
+        el: '#performanceItems',
+        created: function () {
+            var url = _basePath + "/api/TimeSheet/GetManageTimeSheetModel";
+            this.$http.post(url).then(function(response) {
+                this.showUsers[0] = response.data;
+
+                this.showUsers[1] = {};
+                var data = response.data;
+                for (var userId in data) {
+                    this.$set(this.text, userId, this.showTexts[0]);
+                    this.showUsers[1][userId] = [];
+                    for (var rowId in data[userId]) {
+                        if (data[userId][rowId].Status !== "Done") {
+                            var item = {};
+                            $.extend(item, data[userId][rowId])
+                            this.showUsers[1][userId].push(item);
+                        }
+                    }
+                }
+
+                for (var userId in this.showUsers[1]) {
+                    this.$set(this.users, userId, this.showUsers[1][userId]);
+                }
+            }, function(error) { });
+
+            var names = this.names;
+            url = _basePath + "/api/user/GetUserNames";
+            this.$http.post(url).then(function(response) {
+                this.names = response.data;
+            }, function(error) { });
+        },
+        data: {
+            showUsers: {
+                0: {},
+                1: {}
+            },
+            users: {},
+            names: {},
+            showTexts: {
+                0: "Show All",
+                1: "Show Recently"
+            },
+            text: {}
+        },
+        methods: {
+            switchShowAll: function (userId) {
+                if (this.text[userId] === this.showTexts[0]) {
+                    this.text[userId] = this.showTexts[1];
+                    this.users[userId] = this.showUsers[0][userId];
+                }
+                else {
+                    this.text[userId] = this.showTexts[0];
+                    this.users[userId] = this.showUsers[1][userId];
+                }
+            },
+            getTimeSheetId: function (monday) {
+                return utility.getTimeSheetId(monday);
+            },
+            editOrRead: function (status, monday, userId) {
+                return (status === "Done" ? "Read" : "Edit") + "TimeSheet?monday=" + this.getTimeSheetId(monday) + "&userId=" + userId;
+            }
+        }
+    })
+});
