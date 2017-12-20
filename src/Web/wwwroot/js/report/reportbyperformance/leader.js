0587e6548b08cb7899be70108e7e9c2ef20352c8
Bill Wang <billwang@skysznmac025.*.ctriptravel.com>
2017-12-20 18:54:23 +0800

    update code

--- src/Web/wwwroot/js/report/reportbyperformance/leader.js
+++ src/Web/wwwroot/js/report/reportbyperformance/leader.js
@@ -1,15 +1,4 @@
 ﻿$(function () {
-    $("body").on("click", "a.return", function (evt) {
-        //evt.stopPropagation();
-        evt.stopImmediatePropagation();
-        var element = $(this);
-        bootbox.confirm("Are you sure to return this timesheet?", function (result) {
-            if (result) {
-                location = _basePath + "/TimeSheet/ReturnTimeSheet?monday=" + element.attr("data-monday") + "&userId=" + element.attr("data-userId");
-            }
-        });
-    });
-
     var users = new Vue({
         el: '#performanceItems',
         created: function () {
